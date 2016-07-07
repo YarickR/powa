871e8d222b8e4205783b6d4586d31029165833a0
Yaroslav Rastrigin <rastrigin@corp.mail.ru>
2016-07-08 01:33:26 +0300

    Lobby

---
+++ Client/Assets/MatchRow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MatchRow : MonoBehaviour {
+	public int MatchId { get; set ; }
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void JoinButtonClick() {
+		GameObject __o =  GameObject.Find("Canvas/Lobby");
+		Lobby __l = __o.GetComponent<Lobby>();
+		__l.JoinMatch(MatchId);
+	}
+}
